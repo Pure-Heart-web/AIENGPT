188e5f521d489bfe14ff0099a70c39822394bdff
Pure-Heart-web <123462029+Pure-Heart-web@users.noreply.github.com>
2024-10-09 21:09:23 +0800

    删除了当前目录下的所有文件

--- model/embedding/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/3/2 15:28
-# @Author  : nongbin
-# @FileName: __init__.py.py
-# @Software: PyCharm
-# @Affiliation: tfswufe.edu.cn
--- qa/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/2/18 12:16
-# @Author  : nongbin
-# @FileName: __init__.py.py
-# @Software: PyCharm
-# @Affiliation: tfswufe.edu.cn
